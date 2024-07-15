f10214927bb491690a3832b23fc88c42dce65ba9
Marcos Gil <marcosgildeoliveira@gmail.com>
2024-07-15 15:05:40 -0300

    minor changes

--- src/SpatialLightModulator.jl
+++ src/SpatialLightModulator.jl
@@ -16,20 +16,20 @@ struct SLM
 end
 
 function SLM(monitor_id=length(GLFW.GetMonitors()))
+    GLMakie.activate!()
     monitor = GLFW.GetMonitors()[monitor_id]
     video_mode = GLFW.GetVideoMode(monitor)
     width, height = video_mode.width, video_mode.height
     framerate = video_mode.refreshrate
-    GLMakie.activate!(; decorated=false, focus_on_show=true, monitor, framerate)
     fig = Figure(size=(width, height), figure_padding=0)
     ax = Axis(fig[1, 1], aspect=DataAspect())
     hidedecorations!(ax)
     hm = heatmap!(ax, zeros(UInt8, width, height), colormap=:greys, colorrange=(0, 255))
-    screen = display(fig)
+    screen = display(fig; decorated=false, focus_on_show=true, monitor, framerate)
     SLM(monitor, screen, height, width, framerate, fig, ax, hm)
 end
 
-update_hologram!(slm::SLM, hologram::Matrix{UInt8}) = slm.hm[3][] = hologram
+update_hologram!(slm::SLM, hologram::AbstractMatrix{UInt8}) = slm.hm[3][] = hologram
 Base.close(slm::SLM) = GLMakie.destroy!(slm.screen)
 
 end
